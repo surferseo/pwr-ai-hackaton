535e0fc24c53ea0e6629c133be46d373272503d5
Maciej Gruszczyński <maciejgruszczynski@surferseo.com>
2021-10-21 19:34:10 +0200

    Fix export script

--- export.py
+++ export.py
@@ -7,10 +7,11 @@ def call(cmd):
 
 
 bucket_name = "pwr_hackaton"
-source_table = "data_table"
-target_file = "data"
+source_table = "data_content_unstructured"
+target_file = "data_content_unstructured"
+is_large = False
 
-extract_cmd = f"bq extract --compression GZIP --destination_format CSV --noprint_header '{bucket_name}.{source_table}' gs://{bucket_name}/{target_file}-*.csv.gz"
+extract_cmd = f"bq extract --compression GZIP --destination_format CSV --noprint_header {bucket_name}.{source_table} gs://{bucket_name}/{target_file}-*.csv.gz"
 first_files = [
     f"gs://{bucket_name}/{target_file}-00000000000{i}.csv.gz" for i in range(10)
 ]
@@ -18,18 +19,20 @@ first_files_str = " ".join(first_files)
 tmp_file = f"gs://{bucket_name}/{target_file}-000000000100.csv.gz"
 first_files_compose_cmd = f"gsutil compose {first_files_str} {tmp_file}"
 first_files_delete_cmd = f"gsutil rm {first_files_str}"
-all_files_compose_cmd = f"gsutil compose gs://{bucket_name}/{target_file}-*.csv.gz gs://p{bucket_name}/{target_file}.csv.gz"
+all_files_compose_cmd = f"gsutil compose gs://{bucket_name}/{target_file}-*.csv.gz gs://{bucket_name}/{target_file}.csv.gz"
 first_files_delete_cmd = f"gsutil rm {first_files_str}"
 all_files_delete_cmd = f"gsutil rm gs://{bucket_name}/{target_file}-*.csv.gz"
 download_cmd = f"gsutil cp gs://{bucket_name}/{target_file}.csv.gz ."
 
+
 pipeline = [
-    extract_cmd,
-    first_files_compose_cmd,
-    first_files_delete_cmd,
-    all_files_compose_cmd,
-    all_files_delete_cmd,
-    download_cmd,
+    (extract_cmd, True),
+    (first_files_compose_cmd, is_large),
+    (first_files_delete_cmd, is_large),
+    (all_files_compose_cmd, True),
+    (all_files_delete_cmd, True),
+    (download_cmd, True),
 ]
-for cmd in pipeline:
-    call(cmd)
+for cmd, include in pipeline:
+    if include:
+        call(cmd)
